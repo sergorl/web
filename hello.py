c341e2df4e5fb6063287f7e9528c39bb217bd40a
sergorl <serg.neworl@gmail.com>
2019-06-30 21:21:31 +0300

    first

--- hello.py
+++ hello.py
@@ -1,15 +1,14 @@
-import re
+from cgi import parse_qs
 
 
 def app(environ, start_response):
 
-    content = [v + '\n' for v in re.findall(r'(\w+\=\w+)', environ['QUERY_STRING'])]
+    qs = parse_qs(environ['QUERY_STRING'])
 
-    status = '200 OK'
     response_headers = [
         ('Content-type', 'text/plain'),
-        ('Content-Length', str(len(content)))
     ]
-    start_response(status, response_headers)
+    start_response('200 OK', response_headers)
 
-    return [bytes(content, 'utf-8')]
+    return ['%s=%s<br>' % (k, qs[k][0]) fro k in qs]
+    
