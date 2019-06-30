03304a2c1119b822a2d96adc321618eff62e1ad7
sergorl <serg.neworl@gmail.com>
2019-06-30 22:09:31 +0300

    first

--- init.sh
+++ init.sh
@@ -2,8 +2,8 @@
 mkdir ../etc 
 sudo mv -f ./etc/hello.py /home/box/etc/
 
-sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 
 sudo ln -sf /home/box/etc/hello.py /etc/gunicorn.d/hello.py
 
