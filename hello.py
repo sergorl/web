f3554a3397861314ca2f01fce5deccda5c4e6395
sergorl <serg.neworl@gmail.com>
2019-06-30 21:35:18 +0300

    first

--- hello.py
+++ hello.py
@@ -3,7 +3,11 @@ import re
 
 def app(environ, start_response):
 
-    content = [v for v in re.findall(r'(\w+\=\w+)', environ['QUERY_STRING'])]
+	content = []
+    for matches in re.findall(r'(\w+\=\w+)', environ['QUERY_STRING']):
+    	for m in matches:
+    		content += m
+    	m += '\n'	
 
     status = '200 OK'
     response_headers = [
@@ -13,4 +17,3 @@ def app(environ, start_response):
     start_response(status, response_headers)
 
     return iter(content)
-    
