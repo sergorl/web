2e89771e1f06f43f527d74a11c91014c9f03e00a
sergorl <serg.neworl@gmail.com>
2019-06-30 22:32:09 +0300

    first

--- hello.py
+++ hello.py
@@ -6,8 +6,7 @@ def app(environ, start_response):
     body = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
 
     response_headers = [
-        ('Content-type', 'text/plain'),
-        ('Content-Length', str(len(body)))
+        ('Content-type', 'text/plain')
     ]
     start_response('200 OK', response_headers)
 
